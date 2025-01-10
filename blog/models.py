6ec4ecd2ed2f0f48383ddd5c288d755a2568e4a4
Alonsoo907 <itslopez05@gmail.com>
2025-01-10 17:16:33 -0400

    algo

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,12 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
-    cedula = models.CharField(max_length=9, unique=True)
     is_driver = models.BooleanField(default=False)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=200)
 
     def str(self):
         return f"{self.name} {self.lastname}"
-
 
 class Driver(models.Model):
     car_model = models.CharField(max_length=200)
